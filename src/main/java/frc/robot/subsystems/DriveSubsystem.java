799ce8ce66b467e07c0e26a2cf5609805463fd5a
kszed <82227471+kszed@users.noreply.github.com>
2021-08-04 21:28:21 -0400

    partial method header planning

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,5 +8,17 @@ public class DriveSubsystem extends SubsystemBase {
   public DriveSubsystem() {
   
   }
+  //not fulling planned out yet; things to decide: whether we need LDrive and RDrive subsystems, periodic functions vs. default commands
+  
+  
+  public void arcade(    ){
+    //sets drive motors to drive arcade based on parameters
+  }
+  
+  public void aim(){
+    //uses the limelight to aim the robot at the goal
+  }
+  
+  //other functions for auton: driving straight a certain distance (inches) based on parameter, turning certain angle (degrees) based on parameter
 
 }
