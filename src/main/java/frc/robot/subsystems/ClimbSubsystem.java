94d777d97660c607383476904a3b89fc8147c33d
kszed <82227471+kszed@users.noreply.github.com>
2021-08-04 21:15:19 -0400

    method header planning

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -5,4 +5,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ClimbSubsystem extends SubsystemBase {
   
   
+  public void releaseHook(){
+    //retracts the tiny piston to extend the hooks
+  }
+  
+  public void toggleClimb(){
+    //toggles the state of the climb pistons
+  }
+  
 }
