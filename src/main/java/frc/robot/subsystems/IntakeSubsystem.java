3d19e7ae8f7d367f919ccf9da67645f3d456081e
kszed <82227471+kszed@users.noreply.github.com>
2021-08-04 21:03:01 -0400

    method header planning

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,4 +5,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class IntakeSubsystem extends SubsystemBase {
   
   
+  public void intake(double spd){
+    //sets the intake roller motor at speed spd
+  }
+  
+  public void intakePiston(){
+    //toggles the intake pistons between in and out position
+  }
+  
 }
