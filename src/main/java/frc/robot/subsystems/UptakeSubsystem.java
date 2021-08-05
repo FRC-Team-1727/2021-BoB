8c329406006cdb69b463c0c5c2ae30f27858590b
kszed <82227471+kszed@users.noreply.github.com>
2021-08-04 21:09:39 -0400

    method header planning

--- src/main/java/frc/robot/subsystems/UptakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/UptakeSubsystem.java
@@ -5,4 +5,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class UptakeSubsystem extends SubsystemBase {
   
   
+  public void uptake(double spd){
+    //sets v-belt and vertical uptake (feed) motors at speed spd
+    //multipliers can be applied to different parts for different speeds
+  }
 }
