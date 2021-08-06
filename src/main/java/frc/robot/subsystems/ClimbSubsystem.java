c7044be60a9c7eecb8b949dc5c75ed47d7580357
kszed <82227471+kszed@users.noreply.github.com>
2021-08-06 17:17:35 -0400

    simplified constants

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,19 +1,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ClimbConstants;
+import static frc.robot.Constants.ClimbConstants.*;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 public class ClimbSubsystem extends SubsystemBase {
   
-  private DoubleSolenoid hook = new DoubleSolenoid(ClimbConstants.kHookPorts[0], ClimbConstants.kHookPorts[1]);
-  private DoubleSolenoid[] climb = new DoubleSolenoid[] {new DoubleSolenoid(ClimbConstants.kClimbPorts1[0],
-                                                                            ClimbConstants.kClimbPorts1[1]),
-                                                         new DoubleSolenoid(ClimbConstants.kClimbPorts2[0],
-                                                                            ClimbConstants.kClimbPorts2[1]) }
+  private DoubleSolenoid hook = new DoubleSolenoid(kHookPorts[0], kHookPorts[1]);
+  private DoubleSolenoid[] climb = new DoubleSolenoid[] {new DoubleSolenoid(kClimbPorts1[0],
+                                                                            kClimbPorts1[1]),
+                                                         new DoubleSolenoid(kClimbPorts2[0],
+                                                                            kClimbPorts2[1]) }
   //initializes pistons in reverse position so that we can use toggle() method
   for (DoubleSolenoid s : climb){
     s.set(kReverse);
