cd6355640223f138308825e5ed32c881a4be71c8
kszed <82227471+kszed@users.noreply.github.com>
2021-08-06 17:21:47 -0400

    simplified constants

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,18 +1,17 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.IntakeConstants;
+import static frc.robot.Constants.IntakeConstants.*;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 public class IntakeSubsystem extends SubsystemBase {
-  private VictorSPX intake = new VictorSPX(IntakeConstants.kIntakePort);
-  private DoubleSolenoid intakePiston = new DoubleSolenoid(IntakeConstants.kIntakePistonPort[0], IntakeConstants.kIntakePistonPort[1]);
+  private VictorSPX intake = new VictorSPX(kIntakePort);
+  private DoubleSolenoid intakePiston = new DoubleSolenoid(kIntakePistonPort[0], kIntakePistonPort[1]);
   //initializes intakePiston to start in the reverse position so that we can use toggle() method
   intakePiston.set(kReverse);
   
