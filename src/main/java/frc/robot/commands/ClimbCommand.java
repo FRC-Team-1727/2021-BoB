edfd2f8234fca755c52e1e34bab2bd41a1139a33
kszed <82227471+kszed@users.noreply.github.com>
2021-08-09 19:29:02 -0400

    Create ClimbCommand.java

---
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ClimbSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class HookReleaseCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final ClimbSubsystem m_climbSubsystem;
+
+  /**
+   * @param subsystem The subsystem used by this command.
+   */
+  public ClimbCommand(ClimbSubsystem subsystem) {
+    m_climbSubsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_climbSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_climbSubsystem.toggleClimb();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
