7760651b5f3898cce1d19de4e0d6a732e896f34e
kszed <82227471+kszed@users.noreply.github.com>
2021-08-09 19:59:23 -0400

    climb bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,18 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import static edu.wpi.first.wpilibj.XboxController.Button;
+
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.IntakeMotorCommand;
+import frc.robot.commands.IntakePistonCommand;
+import frc.robot.commands.HookReleaseCommand;
+import frc.robot.commands.ClimbCommand;
 
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystem.IntakeSubsystem;
+import frc.robot.subsystem.ClimbSubsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -23,6 +31,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
+  private final CimbSubsystem m_climbSubsystem = new ClimbSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   
@@ -41,9 +50,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    
-    //new JoystickButton (xbox
-    
+    //climb bindings
+    new JoystickButton(xbox, Button.kX.value).whenPressed(new HookReleaseCommand(m_climbSubsystem));
+    new JoystickButton(xbox, Button.kY.value).whenPressed(new ClimbCommand(m_climbSubsystem));
+      
   }
 
   /**
@@ -57,7 +67,7 @@ public class RobotContainer {
   }
 }
 /*
-ran into problem: it doesn't seem like a JoystickButton can be created with an xbox trigger since the 
+ran into problem for intake: it doesn't seem like a JoystickButton can be created with an xbox trigger since the 
 trigger returns a range of double values 0.0-1.0 based on how far it is pressed down instead of being
 a button, so we may need to create a custom trigger - need to look into this further
 
