ca1964ee3bbeda893f7990a6172cf87297c61523
kszed <82227471+kszed@users.noreply.github.com>
2021-08-11 15:49:27 -0400

    adjusted for default command

--- src/main/java/frc/robot/commands/IntakeMotorCommand.java
+++ src/main/java/frc/robot/commands/IntakeMotorCommand.java
@@ -30,9 +30,7 @@ public class IntakeMotorCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_intakeSubsystem.intake(0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
