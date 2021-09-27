6b7f6c1543695be0011734f2f74f961d968f769f
GeoF0945 <86170124+GeoF0945@users.noreply.github.com>
2021-09-27 16:36:19 -0400

    Create UptakeCommand.java
    Modified version of Intake Motor Command

---
+++ src/main/java/frc/robot/commands/UptakeCommand.java
@@ -0,0 +1,49 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.UptakeSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import static frc.robot.Constants.XBoxConstants.*;
+
+public class UptakeMotorCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final UptakeSubsystem m_uptakeSubsystem;
+  private double speed;
+
+  /*
+   * @param subsystem The subsystem used by this command.
+   */
+  public IntakeMotorCommand(IntakeSubsystem subsystem, double spd) {
+    m_uptakeSubsystem = subsystem;
+    if(spd <= 0){
+      if(spd > -kTriggerThreshold){
+        speed = 0;
+      }else{
+        speed = -1;
+      }
+    }else{
+      speed = spd;
+    }
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_uptakeSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_uptakeSubsystem.uptake(speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
