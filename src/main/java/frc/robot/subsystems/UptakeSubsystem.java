9df3592158311db2f74565de11561ee1b075007c
GeoF0945 <86170124+GeoF0945@users.noreply.github.com>
2021-09-27 16:26:26 -0400

    Update UptakeSubsystem.java

--- src/main/java/frc/robot/subsystems/UptakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/UptakeSubsystem.java
@@ -1,12 +1,21 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.UptakeConstants.*;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class UptakeSubsystem extends SubsystemBase {
   
+  private VictorSPX uptake = new VictorSPX(kUptakePort);
+  
+  public UptakeSubsystem(){}
   
   public void uptake(double spd){
     //sets v-belt and vertical uptake (feed) motors at speed spd
     //multipliers can be applied to different parts for different speeds
+    uptake.set(ControlMode.PercentOutput, spd);
   }
+  
 }
