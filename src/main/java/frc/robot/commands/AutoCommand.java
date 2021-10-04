1350a2d2e93b1ce32e6f710736027ebc1cebbe97
GeoF0945 <86170124+GeoF0945@users.noreply.github.com>
2021-10-04 15:29:37 -0400

    auton drive and turn functions

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -3,18 +3,30 @@ package frc.robot.commands;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase; 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class AutoCommand {
     public void driveForward(int distance, int speed){
         double error = 10;
 
         while (error > autonError){
-            lError = distance - lEncoder;
-            rError = distance - rEncoder;
-            setLDrive = (lError * speed) * autonKP;
-            setRDrive = (rError * speed) * autonKP;
+            double lError = distance - lEncoder;
+            double rError = distance - rEncoder;
+            setDrive(lError * speed * autonKP,rError * speed * autonKP);
             error = (lError + rError)/2;
         }
     }
+    
+    public void turn(int distance, int speed){
+        double error = 10;
+
+        while (error > autonError){
+            double lError = distance - lEncoder;
+            double rError = distance - rEncoder;
+            setDrive(-lError * speed * autonKP,rError * speed * autonKP);
+            error = (lError + rError)/2;
+        }
+    }
+    
+    
 }
