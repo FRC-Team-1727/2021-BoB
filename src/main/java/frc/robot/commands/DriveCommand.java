89ce5e771d7ac4e339f530d11f0387b16f79160f
kszed <82227471+kszed@users.noreply.github.com>
2021-10-13 14:18:11 -0400

    implemented DoubleSupplier

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -2,26 +2,27 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-//import java.util.function.DoubleSupplier;
+import java.util.function.DoubleSupplier;
 
 public class DriveCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DriveSubsystem m_driveSubsystem;
   private double yAxis;
   private double xAxis;
-//   private final DoubleSupplier m_forward;
-//   private final DoubleSupplier m_rotation;
+  private final DoubleSupplier m_forward;
+  private final DoubleSupplier m_rotation;
 
   /*
    * @param subsystem The subsystem used by this command.
    */
-  public DriveCommand(DriveSubsystem subsystem, double y, double x) {
+  public DriveCommand(DriveSubsystem subsystem,// double y, double x) {
+    DoubleSupplier y, DoubleSupplier x){
     //replace paramenters with DoubleSupplier y, DoubleSupplier x
     m_driveSubsystem = subsystem;
-    yAxis = y;
-    xAxis = x;
-//     m_forward = y;
-//     m_rotation = x;
+    // yAxis = y;
+    // xAxis = x;
+     m_forward = y;
+     m_rotation = x;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_driveSubsystem);
   }
@@ -33,8 +34,8 @@ public class DriveCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_driveSubsystem.arcade(yAxis, xAxis);
-    //m_driveSubsystem.arcadeDrive(m_forward.getAsDouble(), m_rotation.getAsDouble());
+    //m_driveSubsystem.arcade(yAxis, xAxis);
+    m_driveSubsystem.arcade(m_forward.getAsDouble(), m_rotation.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
