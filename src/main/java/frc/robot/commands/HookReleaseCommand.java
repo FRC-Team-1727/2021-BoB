ffd929757f3d086d4ce23e21e81c31d8347b6041
kszed <82227471+kszed@users.noreply.github.com>
2021-10-13 23:02:30 -0400

    parameter to manually set piston to position

--- src/main/java/frc/robot/commands/HookReleaseCommand.java
+++ src/main/java/frc/robot/commands/HookReleaseCommand.java
@@ -6,12 +6,13 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class HookReleaseCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final ClimbSubsystem m_climbSubsystem;
-
+  private int state;
   /**
    * @param subsystem The subsystem used by this command.
    */
-  public HookReleaseCommand(ClimbSubsystem subsystem) {
+  public HookReleaseCommand(ClimbSubsystem subsystem, int st) {
     m_climbSubsystem = subsystem;
+    state = st;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_climbSubsystem);
   }
@@ -19,7 +20,7 @@ public class HookReleaseCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_climbSubsystem.releaseHook();
+    m_climbSubsystem.releaseHook(state);
   }
 
   // Returns true when the command should end.
